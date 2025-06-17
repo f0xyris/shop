16560a28df10811802ac765731f2f9f514cc6f73
antip4uckia <antip4uck.ia@gmail.com>
2025-06-17 20:43:26 +0200

    vercel build

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
+    chunkSizeWarningLimit: 900,
   },
 });
